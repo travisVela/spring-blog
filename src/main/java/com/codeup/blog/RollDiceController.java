49f45891862411a33ceda3987c3fd561de37d862
Travis Vela <jtravisvela@gmail.com>
2019-05-02 16:49:08 -0500

    add solution to rollDice exercise

---
+++ src/main/java/com/codeup/blog/RollDiceController.java
@@ -0,0 +1,44 @@
+package com.codeup.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class RollDiceController {
+
+    @GetMapping("/rolldice")
+    public String showRollDice() {
+        return "rollDice";
+    }
+
+
+    @GetMapping("rolldice/{number}")
+    public String getDiceRoll(Model model, @PathVariable Integer number) {
+        int randomNumber = randomNumber();
+        String notMatch = "Close, but no cigar";
+        String match = "You guessed it!";
+        String response = null;
+
+
+        if (randomNumber == number) {
+            response = match;
+        } else if (randomNumber != number) {
+            response = notMatch;
+        }
+        model.addAttribute("getDiceRoll", true);
+        model.addAttribute("response", response);
+        model.addAttribute("randomNumber", randomNumber);
+        return "rollDice";
+    }
+
+    private int randomNumber() {
+        return (int) (Math.random() * 6) + 1;
+    }
+
+}
