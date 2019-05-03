f010ca78b5f5987bc06986ab68bdc30303205ab6
Travis Vela <jtravisvela@gmail.com>
2019-05-02 21:37:38 -0500

    add show all view solution

--- src/main/java/com/codeup/blog/PostController.java
+++ src/main/java/com/codeup/blog/PostController.java
@@ -20,18 +20,20 @@ public class PostController {
     @GetMapping("/index")
     public String showAll(Model model) {
         List<Post> posts = new ArrayList<>();
-        String title = "Post # 2";
-        String body = "These things happen automatically. All you have to do is just let them happen. Now we don't want him to get lonely, so we'll give him a little friend. There isn't a rule. You just practice and find out which way works best for you. With practice comes confidence. A fan brush can be your best friend. If you don't think every day is a good day - try missing a few. You'll see.";
+        String post2Title = "Post # 2";
+        String post2Body = "These things happen automatically. All you have to do is just let them happen. Now we don't want him to get lonely, so we'll give him a little friend. There isn't a rule. You just practice and find out which way works best for you. With practice comes confidence. A fan brush can be your best friend. If you don't think every day is a good day - try missing a few. You'll see.";
+        String title = "Hello World";
+        String body = "This is the first post. This is filler test to take up as much space as I can for the first post. Posts are usually about something more interesting, but I don't want to write anything. I would rather post about posting.";
 
-        post2.setTitle(title);
-        post2.setBody(body);
+        post.setTitle(title);
+        post.setBody(body);
+        post2.setTitle(post2Title);
+        post2.setBody(post2Body);
 
         posts.add(post);
         posts.add(post2);
 
         model.addAttribute("posts", posts);
-        model.addAttribute("title", title);
-        model.addAttribute("body", body);
         return "posts/index";
     }
 
